4537bdc035a1a2816b26d95da40179fa6ab0b8df
Abhinav Sharma <abhi18av@gmail.com>
2017-01-15 11:08:51 +0530

    one for the WallBuilders

--- CardVerse.fsx
+++ CardVerse.fsx
@@ -234,30 +234,30 @@ module DefiningClasses =
 
 module DefiningGenericClasses =
 
-    type StateTracker<'T>(initialElement: 'T) = // 'T is the type parameter for the class
+    type WallBuilder<'T>(initialElement: 'T) = // 'T is the type parameter for the class
 
         /// Store the states in an array
-        let mutable states = [ initialElement ]
+        let mutable heights = [ initialElement ]
 
         /// Add a new element to the list of states
-        member this.UpdateState newState =
-            states <- newState :: states  // use the '<-' operator to mutate the value
+        member this.UpdateHeightOfWall newHeightOfWall =
+            heights <- newHeightOfWall :: heights  // use the '<-' operator to mutate the value
 
 
         /// Get the entire list of historical states
-        member this.History = states
+        member this.PreviousHeightsOfWall = heights
 
  
         /// Get the latest state
-        member this.Current = states.Head
+        member this.CurrentHeightOfWall = heights.Head
 
 
     /// An 'int' instance of the state tracker class. Note that the type parameter is inferred.
-    let tracker = StateTracker 10
+    let tracker = WallBuilder 10
 
 
     // Add a state
-    tracker.UpdateState 17
+    tracker.UpdateHeightOfWall 17
 
 
 
